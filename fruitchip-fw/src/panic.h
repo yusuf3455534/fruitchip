cc242da9bdb9a43444030f5cc82267da5d9d6756
awakening <sunlightlucid@gmail.com>
2025-10-21 23:20:32 +0700

    include: move led color macros to its own header

--- fruitchip-fw/src/panic.h
+++ fruitchip-fw/src/panic.h
@@ -1,8 +1,7 @@
-#include <pico/status_led.h>
+#pragma once
+
 #include <pico/platform/panic.h>
 
-#define panic_with_led(color, ...)  { colored_status_led_set_on_with_color(color); panic(__VA_ARGS__); }
+#include "led_color.h"
 
-#define RGB_OK           PICO_COLORED_STATUS_LED_COLOR_FROM_RGB(0x00, 0x10, 0x00) // green
-#define RGB_ERR_APPS     PICO_COLORED_STATUS_LED_COLOR_FROM_RGB(0x10, 0x00, 0x00) // red
-#define RGB_ERR_SETTINGS PICO_COLORED_STATUS_LED_COLOR_FROM_RGB(0x00, 0x00, 0x10) // blue
+#define panic_with_led(color, ...)  { colored_status_led_set_on_with_color(color); panic(__VA_ARGS__); }
---
+++ fruitchip-include/led_color.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <pico/status_led.h>
+
+#define RGB_OK            PICO_COLORED_STATUS_LED_COLOR_FROM_RGB(0x00, 0x10, 0x00) // green
+#define RGB_ERR_APPS      PICO_COLORED_STATUS_LED_COLOR_FROM_RGB(0x10, 0x00, 0x00) // red
+#define RGB_ERR_SETTINGS  PICO_COLORED_STATUS_LED_COLOR_FROM_RGB(0x00, 0x00, 0x10) // blue
