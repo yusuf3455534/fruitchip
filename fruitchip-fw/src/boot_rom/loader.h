36904c388caec68f2698233f40f862877c058e9c
awakening <sunlightlucid@gmail.com>
2025-11-11 01:48:42 +0700

    fw: embed menu

--- fruitchip-fw/src/boot_rom/loader.h
+++ fruitchip-fw/src/boot_rom/loader.h
@@ -7,3 +7,6 @@ extern const uint32_t LOADER_EE_STAGE_1_SIZE;
 
 extern const uint8_t LOADER_EE_STAGE_2[];
 extern const uint32_t LOADER_EE_STAGE_2_SIZE;
+
+extern const uint8_t LOADER_EE_STAGE_3[];
+extern const uint32_t LOADER_EE_STAGE_3_SIZE;
